1b6f8ce3685972316b556a2d4aa0e53ae51d823b
Chandan Gupta <chandan.engg1@outlook.com>
2023-12-27 21:46:45 +0530

    Product Categories

---
+++ Ecommerce.ProductService/Migrations/20231227154540_ProductCategory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.ProductService.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+         
+
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductCategories_ProductCategoryId",
+                table: "Products",
+                column: "ProductCategoryId",
+                principalTable: "ProductCategories",
+                principalColumn: "Id"
+               );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductCategories_ProductCategoryId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProductCategoryId",
+                table: "Products");
+
+       
+        }
+    }
+}
--- Ecommerce.ProductService/Model/Product.cs
+++ Ecommerce.ProductService/Model/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ecommerce.ProductService.Model
 {
@@ -6,6 +7,8 @@ namespace Ecommerce.ProductService.Model
     {
         [Key]
         public int Id { get; set; }
+        public int ProductCategoryId {  get; set; }
         public string Details { get; set; }
+        public ProductCategory Category { get; set; }
     }
 }
